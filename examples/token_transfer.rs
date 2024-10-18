09632a04db73e52706bc2b1ad7fb8fc4e97d0c88
GeorgeThoppil <georgethoppil2@gmail.com>
2024-10-18 12:50:33 -0400

    adding examples

--- examples/token_transfer.rs
+++ examples/token_transfer.rs
@@ -1,4 +1,8 @@
-use erc3643sdk::RwaClient;
+use cosmrs::crypto::secp256k1::SigningKey;
+use erc3643sdk::{
+    token::request::{TokenInfoRequest, TransferMessageRequest},
+    RwaClient,
+};
 
 #[tokio::main]
 async fn main() -> Result<(), Box<dyn std::error::Error>> {
@@ -9,8 +13,32 @@ async fn main() -> Result<(), Box<dyn std::error::Error>> {
         "identity_address",
         "compliance_address",
     )?;
-    // client.transfer().await;
-    // client.transfer_from().await;
-    // client.is_verified_transfer().await;
+
+    let signer = SigningKey::from_slice(&[/* your private key */])?;
+
+    // Perform a token transfer
+    let transfer_request = TransferMessageRequest {
+        from: "cosmos1sender...".to_string(),
+        to: "cosmos1recipient...".to_string(),
+        amount: 100,
+        signer: signer,
+    };
+    let transfer_result = client.transfer(transfer_request).await?;
+    println!("Transfer hash: {}", transfer_result);
+
+    // Check balance
+    let balance_request = TokenInfoRequest {
+        address: "cosmos1sender...".to_string(),
+    };
+    let balance = client.balance(balance_request).await?;
+    println!("Balance: {}", balance.balance);
+
+    // Get token info
+    let token_info = client.coin_info().await?;
+    println!(
+        "Token name: {}, symbol: {}",
+        token_info.name, token_info.symbol
+    );
+
     Ok(())
 }
